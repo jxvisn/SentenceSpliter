91a5f5b8b87956173d4ca3cfe6e4bc6928bb308b
jxvisn <jxvisn@gmail.com>
2019-12-11 17:06:16 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenceSpliter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //GreetUser
+            Console.WriteLine("What is your name?");
+            var userName = Console.ReadLine();
+            Console.WriteLine("Hello {0}!", userName);
+            Console.WriteLine("Enter a random sentence to be split:");
+            //AskUserForInput
+
+
+        }
+    }
+}
